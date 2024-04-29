ac3c4f1713a921f5a39f3949eab7367eae1da35c
Juan David Herrera <david.parra@rappi.com>
2024-04-28 21:27:21 -0500

    🔥 Hotfix 2.0.9
    New web preview image and index typo fix

--- personal_web/constants.py
+++ personal_web/constants.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 # APP Version
-version = "2.0.8"
+version = "2.0.9"
 REPO_URL = "https://github.com/juandaherrera/personal_web"
 
 # Personal
@@ -13,7 +13,7 @@ META = [
 ]
 
 INDEX_TILE = "Juan David Herrera | Web Personal"
-INDEX_DESCRIPTION = f"Portafolio persona de Juan David Herrera © {date.today().year}"
+INDEX_DESCRIPTION = f"Portafolio personal de Juan David Herrera © {date.today().year}"
 INDEX_PREVIEW = "img/web_preview.webp"
 
 INDEX_META = [
